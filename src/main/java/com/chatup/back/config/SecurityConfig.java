e990445a4e799f4d89bacefb2e028bb1db0b90fb
Haeun Jeong <gkdms4248@gmail.com>
2024-03-27 18:00:13 +0900

    fix : port 수정

--- src/main/java/com/chatup/back/config/SecurityConfig.java
+++ src/main/java/com/chatup/back/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
